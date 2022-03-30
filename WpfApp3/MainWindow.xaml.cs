4338ea6caf38535a2869afd095af9a05057bb115
BiBiLog <dzugaev2002@gmail.com>
2022-03-30 09:24:07 +0300

    A

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Login = "admin";
+            var Parol = "admin";
+
+
+            if ((L.Text == Login) && (P.Text == Parol))
+            {
+                Window neww = new Window1();
+                neww.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+                L.Clear();
+                L.Focus();
+            }
+            
+
+        }
+    }
+}
